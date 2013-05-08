a58aaf83ba7d0fcdd5373d166068f8ece25352c4
omegapalguno@gmail.com <omegapalguno@gmail.com@2d144dcf-0bbe-3714-25f6-ea5c0e0c33cb>
2013-05-08 00:45:07 +0000

    report01 & 02

--- spring-hibernate-backend/src/main/java/de/forsthaus/backend/model/TpIdentitas.java
+++ spring-hibernate-backend/src/main/java/de/forsthaus/backend/model/TpIdentitas.java
@@ -5,16 +5,16 @@ import java.util.Date;
 public class TpIdentitas {
 	private int id;
 	private int version;
-	
+
 	private String nip;
 	private String gelar1;
 	private String nama;
 	private String gelar2;
-	
+
 	private String tmLahir;
 	private Date tgLahir;
 	private String kabLahir;
-	
+
 	private String kSex;
 	private String kAgama;
 	private String kStaPeg;
@@ -23,7 +23,7 @@ public class TpIdentitas {
 	private String kStaWin;
 	private String suku;
 	private String kGolDar;
-	
+
 	private String alJalan;
 	private String alRt;
 	private String alRw;
@@ -33,305 +33,478 @@ public class TpIdentitas {
 	private String alKKab;
 	private String alKKec;
 	private String alKDes;
-	
+
 	private String nKarPeg;
 	private String nAsKes;
 	private String nTasPen;
 	private String nKarIs;
 	private String npwp;
 	private String noPen;
-	
+
 	private String jumJiwa;
 	private String jumTang;
 	private String jumIsMi;
 	private String jumAnak;
-	
+
 	private String kCatMut;
 	private String kHukDis;
 	private String kJasa;
 	private String dp3Akhir;
 	private String nipIsMi;
 	private String nipLama;
-	
+
 	private String userEnt;
 	private Date tglEnt;
 	private String userEdt;
 	private Date tglEdt;
-	
+
+	private String kDukPnsDesc;
+	private String kJnsPegDesc;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getVersion() {
 		return version;
 	}
+
 	public void setVersion(int version) {
 		this.version = version;
 	}
+
 	public String getNip() {
 		return nip;
 	}
+
 	public void setNip(String nip) {
 		this.nip = nip;
 	}
+
 	public String getGelar1() {
 		return gelar1;
 	}
+
 	public void setGelar1(String gelar1) {
 		this.gelar1 = gelar1;
 	}
+
 	public String getNama() {
 		return nama;
 	}
+
 	public void setNama(String nama) {
 		this.nama = nama;
 	}
+
 	public String getGelar2() {
 		return gelar2;
 	}
+
 	public void setGelar2(String gelar2) {
 		this.gelar2 = gelar2;
 	}
+
 	public String getTmLahir() {
 		return tmLahir;
 	}
+
 	public void setTmLahir(String tmLahir) {
 		this.tmLahir = tmLahir;
 	}
+
 	public Date getTgLahir() {
 		return tgLahir;
 	}
+
 	public void setTgLahir(Date tgLahir) {
 		this.tgLahir = tgLahir;
 	}
+
 	public String getKabLahir() {
 		return kabLahir;
 	}
+
 	public void setKabLahir(String kabLahir) {
 		this.kabLahir = kabLahir;
 	}
+
 	public String getkSex() {
 		return kSex;
 	}
+
 	public void setkSex(String kSex) {
 		this.kSex = kSex;
 	}
+
 	public String getkAgama() {
 		return kAgama;
 	}
+
 	public void setkAgama(String kAgama) {
 		this.kAgama = kAgama;
 	}
+
 	public String getkStaPeg() {
 		return kStaPeg;
 	}
+
 	public void setkStaPeg(String kStaPeg) {
 		this.kStaPeg = kStaPeg;
 	}
+
 	public String getkJnsPeg() {
 		return kJnsPeg;
 	}
+
 	public void setkJnsPeg(String kJnsPeg) {
 		this.kJnsPeg = kJnsPeg;
 	}
+
 	public String getkDukPns() {
 		return kDukPns;
 	}
+
 	public void setkDukPns(String kDukPns) {
 		this.kDukPns = kDukPns;
 	}
+
 	public String getkStaWin() {
 		return kStaWin;
 	}
+
 	public void setkStaWin(String kStaWin) {
 		this.kStaWin = kStaWin;
 	}
+
 	public String getSuku() {
 		return suku;
 	}
+
 	public void setSuku(String suku) {
 		this.suku = suku;
 	}
+
 	public String getkGolDar() {
 		return kGolDar;
 	}
+
 	public void setkGolDar(String kGolDar) {
 		this.kGolDar = kGolDar;
 	}
+
 	public String getAlJalan() {
 		return alJalan;
 	}
+
 	public void setAlJalan(String alJalan) {
 		this.alJalan = alJalan;
 	}
+
 	public String getAlRt() {
 		return alRt;
 	}
+
 	public void setAlRt(String alRt) {
 		this.alRt = alRt;
 	}
+
 	public String getAlRw() {
 		return alRw;
 	}
+
 	public void setAlRw(String alRw) {
 		this.alRw = alRw;
 	}
+
 	public String getKodePos() {
 		return kodePos;
 	}
+
 	public void setKodePos(String kodePos) {
 		this.kodePos = kodePos;
 	}
+
 	public String getAlTelp() {
 		return alTelp;
 	}
+
 	public void setAlTelp(String alTelp) {
 		this.alTelp = alTelp;
 	}
+
 	public String getAlKProp() {
 		return alKProp;
 	}
+
 	public void setAlKProp(String alKProp) {
 		this.alKProp = alKProp;
 	}
+
 	public String getAlKKab() {
 		return alKKab;
 	}
+
 	public void setAlKKab(String alKKab) {
 		this.alKKab = alKKab;
 	}
+
 	public String getAlKKec() {
 		return alKKec;
 	}
+
 	public void setAlKKec(String alKKec) {
 		this.alKKec = alKKec;
 	}
+
 	public String getAlKDes() {
 		return alKDes;
 	}
+
 	public void setAlKDes(String alKDes) {
 		this.alKDes = alKDes;
 	}
+
 	public String getnKarPeg() {
 		return nKarPeg;
 	}
+
 	public void setnKarPeg(String nKarPeg) {
 		this.nKarPeg = nKarPeg;
 	}
+
 	public String getnAsKes() {
 		return nAsKes;
 	}
+
 	public void setnAsKes(String nAsKes) {
 		this.nAsKes = nAsKes;
 	}
+
 	public String getnTasPen() {
 		return nTasPen;
 	}
+
 	public void setnTasPen(String nTasPen) {
 		this.nTasPen = nTasPen;
 	}
+
 	public String getnKarIs() {
 		return nKarIs;
 	}
+
 	public void setnKarIs(String nKarIs) {
 		this.nKarIs = nKarIs;
 	}
+
 	public String getNpwp() {
 		return npwp;
 	}
+
 	public void setNpwp(String npwp) {
 		this.npwp = npwp;
 	}
+
 	public String getNoPen() {
 		return noPen;
 	}
+
 	public void setNoPen(String noPen) {
 		this.noPen = noPen;
 	}
+
 	public String getJumJiwa() {
 		return jumJiwa;
 	}
+
 	public void setJumJiwa(String jumJiwa) {
 		this.jumJiwa = jumJiwa;
 	}
+
 	public String getJumTang() {
 		return jumTang;
 	}
+
 	public void setJumTang(String jumTang) {
 		this.jumTang = jumTang;
 	}
+
 	public String getJumIsMi() {
 		return jumIsMi;
 	}
+
 	public void setJumIsMi(String jumIsMi) {
 		this.jumIsMi = jumIsMi;
 	}
+
 	public String getJumAnak() {
 		return jumAnak;
 	}
+
 	public void setJumAnak(String jumAnak) {
 		this.jumAnak = jumAnak;
 	}
+
 	public String getkCatMut() {
 		return kCatMut;
 	}
+
 	public void setkCatMut(String kCatMut) {
 		this.kCatMut = kCatMut;
 	}
+
 	public String getkHukDis() {
 		return kHukDis;
 	}
+
 	public void setkHukDis(String kHukDis) {
 		this.kHukDis = kHukDis;
 	}
+
 	public String getkJasa() {
 		return kJasa;
 	}
+
 	public void setkJasa(String jasa) {
 		this.kJasa = jasa;
 	}
+
 	public String getDp3Akhir() {
 		return dp3Akhir;
 	}
+
 	public void setDp3Akhir(String dp3Akhir) {
 		this.dp3Akhir = dp3Akhir;
 	}
+
 	public String getNipIsMi() {
 		return nipIsMi;
 	}
+
 	public void setNipIsMi(String nipIsMi) {
 		this.nipIsMi = nipIsMi;
 	}
+
 	public String getNipLama() {
 		return nipLama;
 	}
+
 	public void setNipLama(String nipLama) {
 		this.nipLama = nipLama;
 	}
+
 	public String getUserEnt() {
 		return userEnt;
 	}
+
 	public void setUserEnt(String userEnt) {
 		this.userEnt = userEnt;
 	}
+
 	public Date getTglEnt() {
 		return tglEnt;
 	}
+
 	public void setTglEnt(Date tglEnt) {
 		this.tglEnt = tglEnt;
 	}
+
 	public String getUserEdt() {
 		return userEdt;
 	}
+
 	public void setUserEdt(String userEdt) {
 		this.userEdt = userEdt;
 	}
+
 	public Date getTglEdt() {
 		return tglEdt;
 	}
+
 	public void setTglEdt(Date tglEdt) {
 		this.tglEdt = tglEdt;
 	}
+
+	public String getkDukPnsDesc() {
+		if(kDukPns.equals("01")){
+			kDukPnsDesc = "AKTIF";
+		}else if(kDukPns.equals("02")){
+			kDukPnsDesc = "CUTIL LUAR TANGGUNAN NEGARA (CLTN)";
+		}else if(kDukPns.equals("03")){
+			kDukPnsDesc = "PERPANJANGAN CLTN";
+		}else if(kDukPns.equals("04")){
+			kDukPnsDesc = "TUGAS BELAJAR";
+		}else if(kDukPns.equals("05")){
+			kDukPnsDesc = "PEMBERHENTIAN SEMENTARA";
+		}else if(kDukPns.equals("06")){
+			kDukPnsDesc = "PENERIMA UANG TUNGGU";
+		}else if(kDukPns.equals("07")){
+			kDukPnsDesc = "WAJIB MILITER";
+		}else if(kDukPns.equals("08")){
+			kDukPnsDesc = "PNS YANG DINYATAKAN HILANG";
+		}else if(kDukPns.equals("09")){
+			kDukPnsDesc = "PEJABAT NEGARA";
+		}else if(kDukPns.equals("10")){
+			kDukPnsDesc = "KEPALA DESA";
+		}else if(kDukPns.equals("11")){
+			kDukPnsDesc = "KEBERATAN ATAS PENJATUHAN HUKUMAN DISIPLIN";
+		}
+		return kDukPnsDesc;
+	}
+
+	public void setkDukPnsDesc(String kDukPnsDesc) {
+		this.kDukPnsDesc = kDukPnsDesc;
+	}
+
+	public String getkJnsPegDesc() {
+		if(kJnsPeg.equals("01")){
+			kJnsPegDesc = "PNS PUSAT DEPDAGRI";
+		}else if(kJnsPeg.equals("02")){
+			kJnsPegDesc = "PNS PUSAT DPB PADA INSTANSI LAIN";
+		}else if(kJnsPeg.equals("03")){
+			kJnsPegDesc = "PNS PUSAT DPK PADA INSTANSI LAIN";
+		}else if(kJnsPeg.equals("04")){
+			kJnsPegDesc = "PNS PUSAT DPB PADA PEMDA PROPINSI";
+		}else if(kJnsPeg.equals("05")){
+			kJnsPegDesc = "PNS PUSAT DPK PADA PEMDA PROPINSI";
+		}else if(kJnsPeg.equals("06")){
+			kJnsPegDesc = "PNS PUSAT DPB PADA PEMDA KOTA";
+		}else if(kJnsPeg.equals("07")){
+			kJnsPegDesc = "PNS PUSAT DPK PADA PEMDA KOTA";
+		}else if(kJnsPeg.equals("08")){
+			kJnsPegDesc = "PNS PUSAT DPB PADA BUMN BADAN LAIN";
+		}else if(kJnsPeg.equals("09")){
+			kJnsPegDesc = "PNS PUSAT DPK PADA BUMN BADAN LAIN";
+		}else if(kJnsPeg.equals("10")){
+			kJnsPegDesc = "PNS DAERAH PROPINSI";
+		}else if(kJnsPeg.equals("11")){
+			kJnsPegDesc = "PNS DAERAH PROPINSI DPB PADA INSTANSI";
+		}else if(kJnsPeg.equals("12")){
+			kJnsPegDesc = "PNS DAERAH PROPINSI DPK PADA INSTANSI";
+		}else if(kJnsPeg.equals("13")){
+			kJnsPegDesc = "PNS DAERAH PROPINSI DPB PADA BUMN/BUMD";
+		}else if(kJnsPeg.equals("14")){
+			kJnsPegDesc = "PNS DAERAH PROPINSI DPK PADA BUMN/BUMD";
+		}else if(kJnsPeg.equals("15")){
+			kJnsPegDesc = "PNS DAERAH KABUPATEN";
+		}else if(kJnsPeg.equals("16")){
+			kJnsPegDesc = "PNS DAERAH KABUPATEN/KOTA DPB PADA INSTANSI";
+		}else if(kJnsPeg.equals("17")){
+			kJnsPegDesc = "PNS DAERAH KABUPATEN/KOTA DPK PADA INSTANSI";
+		}else if(kJnsPeg.equals("18")){
+			kJnsPegDesc = "PNS DAERAH KABUPATEN/KOTA DPB PADA BUMD/BA";
+		}else if(kJnsPeg.equals("19")){
+			kJnsPegDesc = "PNS DAERAH KABUPATEN/KOTA DPB PADA BUMD/BA";
+		}
+		return kJnsPegDesc;
+	}
+
+	public void setkJnsPegDesc(String kJnsPegDesc) {
+		this.kJnsPegDesc = kJnsPegDesc;
+	}
+
 }
